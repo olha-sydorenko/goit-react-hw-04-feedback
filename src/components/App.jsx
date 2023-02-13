430f061e7486b2e4a1a3c871f0205a61e6ec0d35
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-02-13 19:54:06 +0200

    step 1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onHandleGood = () => {
+    this.setState(state => {
+      return {
+        good: state.good + 1,
+      };
+    });
+  };
+  onHandleNeutral = () => {
+    this.setState(state => {
+      return {
+        neutral: state.neutral + 1,
+      };
+    });
+  };
+  onHandleBad = () => {
+    this.setState(state => {
+      return {
+        bad: state.bad + 1,
+      };
+    });
+  };
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    return good ? Math.round((good / (good + neutral + bad)) * 100) : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        <div>
+          <button type="button" onClick={this.onHandleGood}>
+            Good
+          </button>
+          <button type="button" onClick={this.onHandleNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.onHandleBad}>
+            Bad
+          </button>
+        </div>
+        <ul>Statistics</ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {this.countTotalFeedback()}</li>
+        <li>Percentage: {this.countPositiveFeedbackPercentage()} %</li>
+      </>
+    );
+  }
+}
